ce813908dcfa5bee817559b454a154bf95c5a608
HieuNC <hieunc16@gmail.com>
2022-07-29 20:50:02 +0700

    DEVELOP: Change include to require

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-    include 'Player.php';
-    include 'Team.php';
-    include 'Division.php';
-    include 'Game.php';
-    include 'Serie.php';
-    include 'Round.php';
-    include 'HockeyPlayoff.php';
+    require 'Player.php';
+    require 'Team.php';
+    require 'Division.php';
+    require 'Game.php';
+    require 'Serie.php';
+    require 'Round.php';
+    require 'HockeyPlayoff.php';
     $newPlayoffSession = new HockeyPlayoff();
     $newPlayoffSession->simulatePlayoff();
